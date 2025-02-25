ee96d6358172b778a2bc225c093542da669f4d3d
allansgsdev <allanscgomes.dev@gmail.com>
2025-02-25 15:35:03 -0300

    First Commit - Uploading files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// *PÁGINA DE DESAFIOS*
+//
+// Desafio: Aluguel de games
+// Autor: Allan Gomes
+// Curso: Lógica de programação: praticando com desafios
+// Instituição: Alura
+
+// DECLARAÇÃO DE VARIÁVEIS
+
+
+// ATRIBUIÇÃO DE VALORES
+
+
+// DECLARAÇÃO DE FUNÇÕES
+
+
+// PROGRAMA PRINCIPAL
